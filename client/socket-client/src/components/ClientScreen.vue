fab2e3e153d55282e2d1c68b87e54c5719b3ad5c
Supergoose <andrewsargeant@gmail.com>
2018-12-19 08:35:39 +0000

    Added Vue client

---
+++ client/socket-client/src/components/ClientScreen.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="clientscreen">
+    <h1>Hi hi!!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ClientScreen extends Vue {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
